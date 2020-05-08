736cb7b830857162aca415f3a149850c5ad5e9a2
Karna <57597042+Karna1014@users.noreply.github.com>
2020-05-07 21:42:55 -0400

    Update on index

--- src/components/index.js
+++ src/components/index.js
@@ -1,39 +1,45 @@
 import React, { Component } from "react";
-import data from "./emp.json";
+import data from "./Employee.json";
 
 // import "./style.css";
 
-class Table extends Component {
-  // Setting the component's initial state
-  state = {
-      currentSort: 'default'
-  };
 
-  sortTypes = {
+
+
+  const sortTypes = {
       up: {
+          class: "sort-up",
           fn: (a,b) => a.ID - b.ID
       },
       down: {
+          class: "sort-down",
           fn: (a,b) => b.ID - a.ID
       },
       default: {
+          class: "sort",
           fn: (a,b) => a
       }
   }
 
-  getSortOrder(reference) {
-      return function(a,b) {
-          if (a[reference] > b[reference]) {
-              return 1;
-          } else if (a[reference] < b[reference]) {
-              return -1;
-          } else {
-              return 0;
-          }
-      }
-  }
+  class Table extends Component {
+    // Setting the component's initial state
+    state = {
+        currentSort: 'default'
+    };
 
-  sort = (reference) => {
+  // getSortOrder(reference) {
+  //     return function(a,b) {
+  //         if (a[reference] > b[reference]) {
+  //             return 1;
+  //         } else if (a[reference] < b[reference]) {
+  //             return -1;
+  //         } else {
+  //             return 0;
+  //         }
+  //     }
+  // }
+
+  onSortChange = () => {
       const { currentSort } = this.state;
 
       let nextSort;
@@ -48,39 +54,41 @@ class Table extends Component {
   }
 
   render() {
+    const { data } = this.props;
     const {currentSort} = this.state;
 
-    return (
+    return ( data.length > 0 && (
       <div className="container">
         <table className="table table-hover table-striped">
           <thead>
             <tr>
               <th>
-                  <button onClick={this.onSortChange}>ID</button>
+                  <button onClick={this.onSortChange}>
+                    <i className={`fas fa-${sortTypes[currentSort].class}`}></i>ID</button>
               </th>
               <th>First Name</th>
               <th>Last Name</th>
               <th>Email Address</th>
-              <th>Department</th>
+              <th>Title</th>
             </tr>
           </thead>
           <tbody>
             {
-              [...data.data].sort(this.sortTypes[currentSort].fn).map((emp) => (
-                <tr key={emp['ID']}>
-                  <td>{emp['ID']}</td>
-                  <td>{emp['firstName']}</td>
-                  <td>{emp['lastName']}</td>
-                  <td>{emp['Email']}</td>
-                  <td>{emp['Department']}</td>
+              [...data].sort(this.sortTypes[currentSort].fn).map((emp) => (
+                <tr key={ data.id }>
+                  <td>{emp["id"]}</td>
+                  <td>{emp["first_name"]}</td>
+                  <td>{emp["last_name"]}</td>
+                  <td>{emp["email"]}</td>
+                  <td>{emp["title"]}</td>
                 </tr>
-              ))
+              ))}
             }
           </tbody>
         </table>
       </div>
-    );
-  }
-}
+    ))
+    }
+    }
 
-export default Table
+export default Table;
