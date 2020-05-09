6e0552ad22cb78d3f875f64dbd0046e13da3755d
Karna <57597042+Karna1014@users.noreply.github.com>
2020-05-09 07:35:19 -0400

    different direction on code

--- src/components/index.js
+++ src/components/index.js
@@ -1,31 +1,33 @@
-import React, { Component } from "react";
-import data from "./Employee.json";
+// import React, { Component } from "react";
+// import data from "./Employee.json";
 
-// import "./style.css";
+// // import "./style.css";
+// class Table extends React.Component {
+//   // Setting the component's initial state
+//   constructor(props) {
+//     super(props);
+//     this.state = []
+//   };
 
+// componentDidMount() {
 
-
-
-  const sortTypes = {
-      up: {
-          class: "sort-up",
-          fn: (a,b) => a.ID - b.ID
-      },
-      down: {
-          class: "sort-down",
-          fn: (a,b) => b.ID - a.ID
-      },
-      default: {
-          class: "sort",
-          fn: (a,b) => a
-      }
-  }
-
-  class Table extends Component {
-    // Setting the component's initial state
-    state = {
-        currentSort: 'default'
-    };
+//   const sortTypes = {
+//       up: {
+//           class: "sort-up",
+//           fn: (a,b) => a.ID - b.ID
+//       },
+//       down: {
+//           class: "sort-down",
+//           fn: (a,b) => b.ID - a.ID
+//       },
+//       default: {
+//           class: "sort",
+//           fn: (a,b) => a
+//       }
+//   } 
+//     this.state = {
+//         currentSort: 'default'
+//     };
 
   // getSortOrder(reference) {
   //     return function(a,b) {
@@ -39,56 +41,56 @@ import data from "./Employee.json";
   //     }
   // }
 
-  onSortChange = () => {
-      const { currentSort } = this.state;
+  // onSortChange = () => {
+  //     const { currentSort } = this.state;
 
-      let nextSort;
+  //     let nextSort;
 
-      if (currentSort === 'down') nextSort = 'up';
-      else if (currentSort === 'up') nextSort = 'default';
-      else if (currentSort === 'default') nextSort = "down";
+  //     if (currentSort === 'down') nextSort = 'up';
+  //     else if (currentSort === 'up') nextSort = 'default';
+  //     else if (currentSort === 'default') nextSort = "down";
 
-      this.setState({
-          currentSort: nextSort
-      })
-  }
+  //     this.setState({
+  //         currentSort: nextSort
+  //     })
+  // }
 
-  render() {
-    const { data } = this.props;
-    const {currentSort} = this.state;
+  // render() {
+    // const { data } = this.props;
+    // const {currentSort} = this.state;
 
-    return ( data.length > 0 && (
-      <div className="container">
-        <table className="table table-hover table-striped">
-          <thead>
-            <tr>
-              <th>
-                  <button onClick={this.onSortChange}>
-                    <i className={`fas fa-${sortTypes[currentSort].class}`}></i>ID</button>
-              </th>
-              <th>First Name</th>
-              <th>Last Name</th>
-              <th>Email Address</th>
-              <th>Title</th>
-            </tr>
-          </thead>
-          <tbody>
-            {
-              [...data].sort(this.sortTypes[currentSort].fn).map((emp) => (
-                <tr key={ data.id }>
-                  <td>{emp["id"]}</td>
-                  <td>{emp["first_name"]}</td>
-                  <td>{emp["last_name"]}</td>
-                  <td>{emp["email"]}</td>
-                  <td>{emp["title"]}</td>
-                </tr>
-              ))}
-            }
-          </tbody>
-        </table>
-      </div>
-    ))
-    }
-    }
+//     return ( data.length > 0 && (
+//       <div className="container">
+//         <table className="table table-hover table-striped">
+//           <thead>
+//             <tr>
+//               <th>
+//                   <button onClick={this.onSortChange}>
+//                     <i className={`fas fa-${sortTypes[currentSort].class}`}></i>ID</button>
+//               </th>
+//               <th>First Name</th>
+//               <th>Last Name</th>
+//               <th>Email Address</th>
+//               <th>Title</th>
+//             </tr>
+//           </thead>
+//           <tbody>
+//             {
+//               [...data].sort(this.sortTypes[currentSort].fn).map((emp) => (
+//                 <tr key={ data.id }>
+//                   <td>{emp["id"]}</td>
+//                   <td>{emp["first_name"]}</td>
+//                   <td>{emp["last_name"]}</td>
+//                   <td>{emp["email"]}</td>
+//                   <td>{emp["title"]}</td>
+//                 </tr>
+//               ))}
+//             }
+//           </tbody>
+//         </table>
+//       </div>
+//     ))
+//     }; 
+//    }
 
-export default Table;
+// export default Table;
