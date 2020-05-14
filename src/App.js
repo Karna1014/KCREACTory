1194c4f60dac97e65c2b7a7b30e747ea94f95f84
Karna <57597042+Karna1014@users.noreply.github.com>
2020-05-14 11:58:20 -0400

    heroku

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import {HashRouter as Router, Route, Link } from "react-router-dom";
 import EmpTable from "./components/emp-table";
 import data from "./data/Employee.json";
 
