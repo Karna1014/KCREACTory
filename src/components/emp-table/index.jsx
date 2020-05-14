bd470cad590b42d6ada5d89b8a389cbf00c12ba3
Karna <57597042+Karna1014@users.noreply.github.com>
2020-05-14 10:26:19 -0400

    cleaned code, added email to sort

--- src/components/emp-table/index.jsx
+++ src/components/emp-table/index.jsx
@@ -1,43 +1,54 @@
 import React from "react";
 
 export default function EmpTable(props) {
-    return (
-      <div className="container">
-      <nav className="navbar navbar-light bg-light">
-            <span className="navbar-brand">Employee List</span>
-            <form className="form-inline">
-              { <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" onChange={(event) => props.onSearchInputChange(event, event.target.value)}/> }
-              {/* <input className="input mr-sm-2" type="text" placeholder="Search via First Name" aria-label="Search" onChange={handleChange}/> */}
-            </form>
-          </nav>
-        <table>
-            <thead>
-            <tr>
-              <th>
-                <button onClick={(event) => props.sortBy(event, "id")}>ID</button>
-              </th>
-              <th></th>
-              <th>First Name</th>
-              <th>Last Name</th>
-              <th>Email Address</th>
-              <th>Title</th>
+  return (
+    <div className="container">
+      <nav className="navbar navbar-light">
+        <span className="navbar-brand text-white">Employee List</span>
+        <form className="form-inline">
+          {
+            <input
+              className="form-control mr-sm-2"
+              type="search"
+              placeholder="Search by First Name"
+              aria-label="Search"
+              onChange={(event) =>
+                props.onSearchInputChange(event, event.target.value)
+              }
+            />
+          }
+        </form>
+      </nav>
+      <table className="table table-striped table-bordered">
+        <thead>
+          <tr>
+            <th>
+              <button onClick={(event) => props.sortBy(event, "id")}>ID</button>
+            </th>
+            <th>Pic</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>
+              <button onClick={(event) => props.sortBy(event, "email")}>
+                Email Address
+              </button>
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {props.data.map((results) => (
+            <tr key={results.id}>
+              <td>{results.id}</td>
+              <td>
+                <img alt="no pic" src={results.avatar} />
+              </td>
+              <td>{results.first_name}</td>
+              <td>{results.last_name}</td>
+              <td>{results.email}</td>
             </tr>
-          </thead>
-          <tbody>
-              {
-                props.data.map(row => (
-                <tr key={row.id}>
-                    <td>{row.id}</td>
-                    <td><img alt="avatar" src={row.avatar}/></td>
-                    <td>{row.first_name}</td>
-                    <td>{row.last_name}</td>
-                    <td>{row.email}</td>
-                    <td>{row.title}</td>
-                 </tr>
-                 ))
-                }
-          </tbody>
-        </table>
-      </div>
-    )
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
