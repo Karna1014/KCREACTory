8efe8aefb5b1cf71bf61fcacc1053e56b526fad0
Karna <57597042+Karna1014@users.noreply.github.com>
2020-05-14 15:24:03 -0400

    Updates

---
+++ precache-manifest.c33023cca23da30b058b9212c3cf9253.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d4f95df2e49bcfa11f7974ca41dbf2d",
+    "url": "/KCREACTory/index.html"
+  },
+  {
+    "revision": "4f48cdab3b8a69613dbf",
+    "url": "/KCREACTory/static/css/main.ad88b555.chunk.css"
+  },
+  {
+    "revision": "b654b49b6290f5466852",
+    "url": "/KCREACTory/static/js/2.9c9da10e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/KCREACTory/static/js/2.9c9da10e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f48cdab3b8a69613dbf",
+    "url": "/KCREACTory/static/js/main.73d46c28.chunk.js"
+  },
+  {
+    "revision": "735ff48b404a7921ef02",
+    "url": "/KCREACTory/static/js/runtime-main.48ba45e6.js"
+  }
+]);
